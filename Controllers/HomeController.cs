8b8535f5e1c0fa25646e60941d13c62bb2b8ea72
Jen-Chieh-Yu <xlarge840516@gmail.com>
2023-09-28 17:59:09 +0800

    ++

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Homepage()
         {
             return View();
         }
